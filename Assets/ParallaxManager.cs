e8022828d27a022cc74414100048ac45e270afec
etzarov <etz211@nyu.edu>
2020-11-24 13:08:28 -0500

    Added background + parallax effect images+code

---
+++ Assets/ParallaxManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxManager : MonoBehaviour
+{
+
+    public Transform[] spriteTransforms;
+    [Space]
+    public float unityUnitWidth;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitializePositions();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < spriteTransforms.Length; i++)
+        {
+            Vector3 position = spriteTransforms[i].localPosition;
+            position.x = (position.x + speed * Time.deltaTime) % (unityUnitWidth * (float)(spriteTransforms.Length));
+            spriteTransforms[i].localPosition = position;
+        }
+    }
+
+    void InitializePositions()
+    {
+        for (int i = 0; i < spriteTransforms.Length; i++)
+        {
+            Vector3 position = transform.position;
+            position.x = i * unityUnitWidth;
+            spriteTransforms[i].localPosition = position;
+        }
+    }
+}
