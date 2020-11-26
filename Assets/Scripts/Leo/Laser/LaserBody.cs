d0d5a6cae45813f9d2aa97f365efc821aa3cb9d7
Primary <yw3726@nyu.com>
2020-11-25 23:41:50 -0500

    Imported Sprites, made replicatable laser, built outlines.

---
+++ Assets/Scripts/Leo/Laser/LaserBody.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserBody : MonoBehaviour
+{
+    public GameObject LaserBase;
+    public GameObject LaserTail;
+
+    public Vector2 MyScale;
+    public float Length;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.right = LaserTail.transform.position - transform.position;
+        transform.position = LaserBase.transform.position;
+        //MyScale = new Vector2(Vector2.Distance(LaserBase.transform.position, LaserTail.transform.position), 1);
+        Length = Vector2.Distance(LaserBase.transform.position, LaserTail.transform.position)*0.65f;
+        //Length /= transform.lossyScale.y;
+        transform.localScale = new Vector2(Length, LaserBase.transform.lossyScale.y);
+    }
+}
